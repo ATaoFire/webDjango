83aeb7930daec4a3baf26b7b3c7413f0197532a5
zhangtao <zhangtao@aiyunxiao.com>
2019-10-17 10:00:20 +0800

    fenye

--- MyBlog/blog/views.py
+++ MyBlog/blog/views.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from django.views.generic.base import View
 from django.db.models import Q
 #分页机制
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator
 #登录
 from django.contrib.auth import authenticate, login, logout
 #注册
@@ -179,31 +179,7 @@ class TagMessage(object):
         self.name = name
         self.count = count
 
-def blog_list(request,cid=-1, tid=-1):
-
-    """
-    分页器的使用
-    book_list = Book.objects.all()
-    paginator = Paginator(book_list, 10)
-    print("count:", paginator.count)  # 总数居
-    print("num_pages", paginator.num_pages)  # 总页数
-    print("page_range", paginator.page_range)  # 页面的列表
-
-    page_one = paginator.page(1)  # 第一页的page对象
-    for i in range(page_one):  # 遍历第1页的所有数据对象
-        print(i)
-    print(page_one.object_list)  # 第1页所有数据
-
-    page_two = paginator.page(2)
-    print(page_two.has_next())  # 是否有下一页
-    print(page_two.next_page_number())  # 下一页的页码
-    print(page_two.has_previous())  # 是否有上一页
-    print(page_two.previous_page_number())  # 上一页的页码
-
-    # 抛出错误
-    page = paginator.page(12)  # Error:EmptyPage
-    page = paginator.page("z")  # Error:PageNotAnInteger
-    """
+def blog_list(request,cid=-1, tid=-1, pageNum =-1):
     post_list = None
     if cid != -1:
         cat = BlogCategory.objects.get(id=cid)
@@ -214,23 +190,15 @@ def blog_list(request,cid=-1, tid=-1):
     else:
         post_list = Post.objects.all()
 
-<<<<<<< HEAD
     #进行分页
-    paginator = Paginator(post_list, 10)
-    page = request.GET.get("page", 1)
-    current_page = int(page)
-    # post_list = paginator.page(current_page)  # 显示第1页的内容
-
-=======
-    # try:
-    #     page = request.GET.get('page', 1)
-    # except PageNotAnInteger:
-    #     page = 1
-    #
-    # p = Paginator(post_list, per_page=1, request=request)
-    #
-    # post_list = p.page(page)
->>>>>>> 7b71495aac1cd0257a6a3e3e3cf79147f96be96f
+    my_paginator = Paginator(post_list, 1)  # 实例化一个分页对象, 每页显示10个
+    page = 0
+    if pageNum == -1:
+        page = request.GET.get('page', default=1)  # 从URL通过get页码，如?page=3
+    else:
+        page = request.GET.get(pageNum)
+    # 获取某页对应的记录, 如果
+    post_listA = my_paginator.get_page(page)
 
     tags = Tags.objects.all()
     tag_message_list = []
@@ -249,15 +217,10 @@ def blog_list(request,cid=-1, tid=-1):
             post_list1.append(comment.post.id)
 
     ctx = {
-        'post_list': post_list,
+        'post_list': post_listA,
         'tags': tag_message_list,
         'new_comment_list': new_comment_list1,
-<<<<<<< HEAD
-        "page_range": paginator.page_range,
-        "paginator": paginator,
-        "current_page": current_page
-=======
->>>>>>> 7b71495aac1cd0257a6a3e3e3cf79147f96be96f
+        'paginator': my_paginator,
     }
     return render(request, 'list.html', ctx)
 
